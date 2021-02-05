be08859f9dd1cf14e5dcc424e96736de6824196e
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2021-02-04 18:58:13 -0800

    Created a file tree with click to open and close functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+//react component is just a javascript function that returns something to be rendered or "viewed/shown" on screen
+//create a react component is create a js function
+//render VIEW THE COMPONENT ON THE WEBPAGE
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div>
+  <Folder name="Desktop">
+      <Folder name="Music">
+       <File name="star.mp4"/>
+       <File name="music.file.mp4"/>
+      </Folder>
+    <File name="dogs.jpeg"/>
+    <File name="cats.png"/>
+  </Folder>
+  <Folder name="Applications"/>
+  </div>
+}
+//props key concept in react 
+const Folder = (props) => {
+  const [ isOpen, setIsOpen ] = useState(true);
+  const { name, children} = props;
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
+  return <div>
+    <span onClick={handleClick}>{name}</span>
+    
+    <div style={{ marginLeft: '17px'}}>
+    {isOpen ? children: null}
     </div>
-  );
+    </div>
+};
+
+const File = (props) => {
+  return <div>{props.name}</div>
 }
 
 export default App;
