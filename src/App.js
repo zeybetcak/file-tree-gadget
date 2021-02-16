0513150579cdd8cb41e996671303796c9e47c563
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2021-02-15 18:10:07 -0800

    Added click to open feature and as well as icon images next to all files

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   <Folder name="Desktop">
       <Folder name="Music">
        <File name="star.mp4"/>
-       <File name="music.file.mp4"/>
+       <File name="music_file.mp4"/>
       </Folder>
     <File name="dogs.jpeg"/>
     <File name="cats.png"/>
@@ -19,24 +19,38 @@ function App() {
 }
 //props key concept in react 
 const Folder = (props) => {
+  //use state is a hook and all hooks in react potentially rerender components 
   const [ isOpen, setIsOpen ] = useState(true);
   const { name, children} = props;
+  const direction = isOpen ? 'down' : 'right';
 
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  }
+  const handleClick = () => setIsOpen(!isOpen);
 
   return <div>
-    <span onClick={handleClick}>{name}</span>
-    
+    <span onClick={handleClick}><i className="blue folder icon"></i>
+    <i className={`caret ${direction} icon`}></i>
+    </span>
+    {name}
     <div style={{ marginLeft: '17px'}}>
     {isOpen ? children: null}
     </div>
     </div>
-};
+}; 
 
 const File = (props) => {
-  return <div>{props.name}</div>
+  const { name } = props;
+  const fileExtension = name.split('.')[1]
+  const fileIcons = {
+  mp4: 'headphones',
+  jpeg: 'file image',
+  png: 'file image outline',
+};
+
+  return <div>
+    <i className={`${fileIcons[fileExtension]} icon`}></i>
+    {name}
+    </div>
 }
 
 export default App;
+ 
